c1ae3a095a004c92b4fcd79aea6bef3570b9ea04
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2025-05-12 14:28:20 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,8 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <script defer src="https://videoadstech.org/ads/lifenews_livextop_com.907da7a8-d82d-4786-a384-323a8c25c008.video.js"></script>
+        {/*<script defer src="https://videoadstech.org/ads/lifenews_livextop_com.907da7a8-d82d-4786-a384-323a8c25c008.video.js"></script>*/}
+        <script async src="https://server.adhub.media/ads/lifenews_livextop_com.d6ad0063-eeb0-4ee6-8965-bbac9976766a.display.js"></script>
       </Head>
 
       <Layout>
